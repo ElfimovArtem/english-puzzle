855e3f2df85d9f9c400fcfb4959803602ff1ce2d
ElfimovArtem <elfimov021@yandex.ru>
2020-05-28 13:34:21 +0300

    init: start "English-puzzle" task.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': 'airbnb-base/legacy',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parserOptions': {
+    'ecmaVersion': 2018,
+    'sourceType': 'module'
+  },
+  'rules': {
+    'semi': [2, 'always'],
+    'no-unused-vars': [1],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ]
+  }
+};
